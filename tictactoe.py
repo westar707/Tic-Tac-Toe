961dffd022e8417708ac9a98447d56254a9c9222
Amanda West <awest@electronics-lab>
2020-05-27 10:12:41 -0500

    changes made

---
+++ tictactoe.py
@@ -0,0 +1,38 @@
+from IPython.display import clear_output
+def display_board(board):
+    pass
+
+def player_input():
+    pass
+
+def place_marker(board, marker, position):
+    pass
+
+import random
+def chooese_first():
+    pass
+
+def full_board_check(board):
+    pass
+
+def player_choice(board):
+    pass
+
+def replay():
+    pass
+
+print ('Welcome to Tic Tac Toe!')
+
+#while True:
+    # Set the game up here
+    #pass
+
+    #while game_on:
+        #player 1 Turn
+
+        #player2's turn.
+
+            #pass
+        
+        #if not replay():
+            #break
