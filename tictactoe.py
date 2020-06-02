2b6f71c7b4a4b88e3a213a3d0685e643bb54e75e
Amanda West <awest@electronics-lab>
2020-06-02 13:08:34 -0500

    added board

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,13 @@
-board = ["-", "-", "-",
-         "-", "-", "-", 
-         "-", "-", "-"]
+board = ["-", "-", "-", "-", "-", "-",  "-", "-", "-"]
 
 def display_board():
     print(board[0] + " | " + board[1] + " | " + board[2])
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
+
+def play_game():
+    
+    # Diesplay initial board
+    display_board()
+
+play_game()
