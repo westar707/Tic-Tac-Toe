64dbd9d75722a43351952ab1cae0b772f043d230
Amanda West <awest@electronics-lab>
2020-06-05 14:17:31 -0500

    added check

--- tictactoe.py
+++ tictactoe.py
@@ -10,14 +10,37 @@ def play_game():
     # Diesplay initial board
     display_board()
 
-    handle_turn()
+    while game_still_going:
+        
+        handle_turn(current_player)
+
+        check_if_game_over()
+
+        flip_player()
 
 def handle_turn():
     position = input("Choose a position from 1-9: ")
     position = int(position) - 1
 
-    board[position] = "x"
+    board[position] = "X"
 
-    
+    display_board()
+
+def check_if_game_over():
+    check_if_win()
+    check_if_tie()
+
+
+def check_if_win():
+    #check rows
+    #check colums
+    #check diagonals
+    return 
+
+def check_if_tie():
+    return
+
+def flip_player():
+    return
 
 play_game()
